f5d655e2ca261a5599d1f569e4c9dd99b5649b4f
Eduardo Medeiros Silva <edu.medeiros.info@gmail.com>
2022-08-09 16:28:40 +0000

    Added Homebrew integration

---
+++ tools/homebrew-installer/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p /opt/xavier/linuxbrew
+chown 1000:1000 /opt/xavier/linuxbrew
+docker build /opt/xavier/system/tools/homebrew-installer -t homebrew-installer
+docker run --rm -it -v /opt/xavier/homebrew:/home/linuxbrew homebrew-installer
+cp -f /opt/xavier/system/tools/homebrew-installer/homebrew.zsh /opt/xavier/root/.oh-my-zsh/custom/homebrew.zsh
+cp -f /opt/xavier/system/tools/homebrew-installer/homebrew_bash /opt/xavier/homebrew/.bash_profile
+chown 1000:1000 /opt/xavier/homebrew/.bash_profile
